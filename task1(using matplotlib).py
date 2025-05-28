5cca0723562eddf80438c57a7ef53cf04b98ee0c
GOPI M <gopimandapalli143@gmail.com>
2025-05-28 10:49:53 +0530

    task1(using matplotlib).py

--- task1(using matplotlib).py
+++ task1(using matplotlib).py
@@ -3,17 +3,20 @@
 import requests
 import json
 import matplotlib.pyplot as plt
+import key  #import the API_KEY from the key file
 
 #enter city name from the user
 
 CITY_NAME=input("enter the city name :")
-API_KEY="1f8c80bd8d556b0afa21d9bafb9d309c" #intitilize API_KEY in openweathermap.org
+
+ #import the API_KEY from the key
 
 #select API from openweathermap.org and click built-on API request by city name and select the URL
 
-URL=f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={API_KEY}&units=metric"
+URL=f"https://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={key.API_KEY}&units=metric"
 a=requests.get(URL)
 data=a.json()
+#print(data)
 
 #extract temperature and time
 
@@ -23,9 +26,9 @@ time=[]
 #add the temperature and time from the taken city in the openweathermap.org
 
 temperature.append(data['main']['temp'])
-print(temperature)
+print(f"Current Temperature : {temperature}")
 time.append(data["timezone"])
-print(time)
+print(f"Current Time : {time}")
 
 #addig color,size and family to the text
 
@@ -36,8 +39,8 @@ f3={'family':'arial','color':'b','size':20}
 
 print(plt.figure(figsize=(10,5)))#size of figure(x,y)
 print(plt.plot(time,temperature,marker="d",mfc="r",mec="k",ms=15))
-print(plt.title(f"24-Hour Temperature Forecast for {CITY_NAME}",fontdict=f1))
+print(plt.title(f"Current Temperature & Time Forecast for {CITY_NAME}",fontdict=f1))
 print(plt.xlabel("TIME",fontdict=f2))
-print(plt.ylabel("TEMPERATURE",fontdict=f3))
+print(plt.ylabel("TEMPERATURE(0°C)",fontdict=f3))
 print(plt.grid())
 print(plt.show())
